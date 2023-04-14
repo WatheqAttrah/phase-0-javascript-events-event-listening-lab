a7f0c4d616e9944e0c22f262fdec80ce6f6848e5
Watheq Attrah <novaatrah@gmail.com>
2023-04-14 16:45:32 -0700

    complete

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+// function addingEventListener() {
+//     const input=document.getElementById('button');
+//     return input.addEventListener('click',clickAlert)
+// }
+// addingEventListener()
+
+// function clickAlert(){
+//     return alert('I was clicked!');
+// }
+
 function addingEventListener() {
+    const input=document.getElementById('button');
+    return input.addEventListener('click',() => alert('I was clicked'))
 }
+addingEventListener()
+
+// function clickAlert(){
+//     return alert('I was clicked!');
+// }
+
